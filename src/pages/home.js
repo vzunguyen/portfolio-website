320c8edde002585520eb915c69a9dde376d4f2d9
Du Van Nguyen <vandu7124@gmail.com>
2025-03-19 23:36:16 +1100

    update

--- src/pages/home.js
+++ src/pages/home.js
@@ -11,12 +11,15 @@ export default function Home() {
           </h1>
         </div>
         <p class="flex flex-auto mt-5 p-5">
-          I am currently a second year student at Swinburne University of
+          I am currently a third year student at Swinburne University of
           Technology, studying Computer Science with a major in Software
-          Development. I am passionate about technology and problem-solving.
-          Additionally, I am always eager to learn more and improve my skills as
-          it is important to quickly adapt in this everchanging world of
-          technology.
+          Development. Additionally, I'm also an intern at Education Network
+          Group, working in WhizKids Game branch, games for autistic children.
+          Due to my passion for problem-solving and understanding how things
+          work, I have made it my goal to become a software developer. Despite
+          being new to the industry, I am always eager to learn more and improve
+          my skills as I believe it is important to quickly adapt in this
+          everchanging world of technology.
         </p>
       </div>
       <div class="block p-6 m-6 bg-white border border-gray-100 rounded-lg shadow-sm">
